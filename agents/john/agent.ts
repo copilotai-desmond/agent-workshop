533840dd967f4d23f40c9b963bc7d7cc1a2010a1
jvainio <john.vainio@copilotai.com>
2025-02-11 22:29:27 +0000

    Add john

---
+++ agents/john/agent.ts
@@ -0,0 +1,64 @@
+import { END, MemorySaver, START, StateGraph } from "@langchain/langgraph";
+import { desmondsFavoriteCity, favoriteCityNodeName } from "./favoriteCity.node";
+import { State, StateAnnotation } from "./state";
+import { ToolNode, toolsCondition } from "@langchain/langgraph/prebuilt";
+import { GoogleCustomSearch } from "@langchain/community/tools/google_custom_search";
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate, MessagesPlaceholder, SystemMessagePromptTemplate } from "@langchain/core/prompts";
+import { foodRecommendationTool } from "./foodRecommendation.tool";
+
+const prompt = `
+You are an agent specialized in finding information about a human's favorite city.
+Ask the human what their favorite city is and provide them with some information about it.
+The information you need to provide are:
+- The name of the city
+- The average flight cost to the city from the human's location. If you don't know the human's location, ask them for the location.
+- Some tourist attractions in the city
+- Some food recommendations from the get_food_recommendation tool
+
+System time: {systemTime}
+`;
+
+// Define the tools that the agent will use
+const tools = [
+    new GoogleCustomSearch(),
+    foodRecommendationTool,
+    // https://js.langchain.com/docs/integrations/platforms/google#google-search
+];
+const toolNode = new ToolNode(tools);
+
+const llm = new ChatOpenAI({ model: 'gpt-4o-mini' }).bindTools(tools);
+async function agentNode(state: State): Promise<Partial<State>> {
+    // Create a system prompt for the agent
+    const systemPrompt = SystemMessagePromptTemplate.fromTemplate(prompt);
+
+    // Format messages into an array of system and ai / human message
+    const messages = await ChatPromptTemplate.fromMessages([
+        systemPrompt,
+        new MessagesPlaceholder('messages'),
+    ]).partial({
+        systemTime: new Date().toDateString(),
+    });
+
+    // Create a chain that will pipe the list of messages into our llm
+    const chain = messages.pipe(llm);
+
+    // Invoke the chain to get a response
+    const response = await chain.invoke({ messages: state.messages});
+
+    // Update our messages state with the response
+    return { messages: [response] };
+}
+
+const graphBuilder = new StateGraph(StateAnnotation)
+  .addNode('agent', agentNode)
+  .addNode('tools', toolNode)
+  .addNode('favoriteCityRetriever', desmondsFavoriteCity)
+  .addConditionalEdges('agent', toolsCondition, ['tools', END])
+  .addEdge('tools', 'agent')
+  .addEdge(START, 'favoriteCityRetriever')
+  .addEdge('favoriteCityRetriever', 'agent')
+  .addEdge('agent', END);
+
+const checkpointer = new MemorySaver();
+export const agent = graphBuilder.compile({ checkpointer });
