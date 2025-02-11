34502379b17cc0be9884bc9145b0237e6fdf970c
Michael Hofer <michael.hofer@copilotai.com>
2025-02-11 22:20:29 +0000

    mhofer/adds mike's agent

---
+++ agents/mike/favoriteCity.node.ts
@@ -0,0 +1,12 @@
+import { HumanMessage } from "@langchain/core/messages";
+import type { State } from "./state";
+
+function responseFormatter(name: string, favoriteCity: string): HumanMessage {
+    return new HumanMessage(`My name is ${name} and my favorite city is ${favoriteCity}`);
+}
+
+export async function desmondsFavoriteCity(state: State): Promise<Partial<State>> {
+    return { messages: [responseFormatter('Mike', 'San Francisco')]}
+}
+
+export const favoriteCityNodeName = 'favorite-city-retriever';
